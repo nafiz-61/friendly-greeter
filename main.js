de7a7f494fdf7eb664c0268181d061cffa47b9e8
nafiz-61 <nafizzubaer61@gmail.com>
2025-07-16 23:09:25 +0600

    friendly-greeter

---
+++ main.js
@@ -0,0 +1,37 @@
+// function
+function greating(name, surename = "") {
+  if (name == "Eleanor") {
+    const addingName = `Welcome to the world Ms. ${name}${surename}`;
+    alert(addingName);
+  } else {
+    const addingName = `Welcome to the world Mr. ${name}${surename}`;
+    alert(addingName);
+  }
+}
+
+let allFrnd = [
+  "Eleanor",
+  "Knox",
+  "Delilah",
+  "Jasper",
+  "Hawthorne",
+  "Hansen",
+  "Herman",
+  "Ngo",
+  "Ruff",
+  "Kent",
+  "Eller",
+  "Mr.Doyle",
+  "Truitt",
+  "Benner",
+  "Kelleher",
+  "Clinton",
+  "Luther",
+  "Marquez",
+  "Britt",
+  "Dugger",
+];
+
+for (let name of allFrnd) {
+  greating(name);
+}
